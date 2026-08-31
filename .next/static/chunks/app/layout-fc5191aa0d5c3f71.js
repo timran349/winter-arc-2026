d22dad1566413a38730cf5a502fde6f8ea7a935f
Tushar Imran <heytimran@gmail.com>
2026-09-01 01:32:02 +0600

    fix: install @tailwindcss/postcss and add postcss.config.mjs for full Tailwind CSS v4 styling in Next.js

--- .next/static/chunks/app/layout-fc5191aa0d5c3f71.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6893:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7001,23))},7001:()=>{}},e=>{e.O(0,[258,441,255,358],()=>e(e.s=6893)),_N_E=e.O()}]);
